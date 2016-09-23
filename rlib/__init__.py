3d571a1f16f50602ac7eb535165110178a9caddf
Robert Larsen <robert@the-playground.dk>
2016-09-23 22:08:28 +0200

    Removed unneeded import.

--- rlib/__init__.py
+++ rlib/__init__.py
@@ -2,7 +2,6 @@ from influx import influxdb
 from web import urlparse
 import netstring
 from text import hexify, json_pretty
-import realease
 from protocols import parse_packet, parse_IPv4, parse_IPv6, parse_Cooked, parse_Ethernet, parse_UDP, parse_TCP, parse_DNS
 from pcap import pcap_packets
 from util import PubSub
